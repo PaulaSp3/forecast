a6817d75c2f74a8f07052bc143965e250f0c3cb5
lwdtirol <lwd@tirol.gv.at>
2022-06-02 17:47:17 +0200

    Eventlistener für Vorhersagen weltweit bei klick auf Karte

--- main.js
+++ main.js
@@ -104,5 +104,15 @@ async function loadWeather(url) {
 `;
 
 marker.setPopupContent(popup).openPopup();
+
 };
 loadWeather("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=47.267222&lon=11.392778");
+
+//Reaktion bei Klick auf Karte
+map.on("click", function(evt) {
+    console.log(evt);
+
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    loadWeather(url);
+})
+
