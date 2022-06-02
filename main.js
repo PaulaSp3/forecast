49205106a2720b01ee12068ed4b72ccddd42f2d3
lwdtirol <lwd@tirol.gv.at>
2022-06-02 16:19:53 +0200

    Overlay für wind vorhersage zeitpunkt erstellt

--- main.js
+++ main.js
@@ -51,6 +51,8 @@ async function loadWind(url) {
 
     let forecastLabel = formatDate(forecastDate);
     //console.log("Vorhersagezeitpunkt: ", forecastLabel);
+
+    layerControl.addOverlay(overlays.wind, `ECMWF Windorhersage für ${forecastLabel}`)
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
