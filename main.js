682ae39b0fa623a4fde8d7897d85ac91e030e6b8
lwdtirol <lwd@tirol.gv.at>
2022-06-02 16:03:15 +0200

    Vorhersagedatum berechnet (Ortszeit)

--- main.js
+++ main.js
@@ -32,7 +32,12 @@ L.control.scale({
 async function loadWind(url) {
     const response  = await fetch(url);
     const jsondata = await response.json();
-    console.log(jsondata);
+    console.log(jsondata[0].header.refTime);
+    console.log(jsondata[0].header.forecastTime);
+    // UTC in Lokaler Ortszeit
+    let forecastDate = new Date(jsondata[0].header.refTime);
+    forecastDate.setHours(forecastDate.getHours() + jsondata[0].header.forecastTime)
+    console.log(forecastDate);
 };
 loadWind("https://geographie.uibk.ac.at/webmapping/ecmwf/data/wind-10u-10v-europe.json");
 
